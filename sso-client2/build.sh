acca9ae8562b33634aa9da7def8b93c0c7296e81
longfeizheng <longfei_zheng@163.com>
2018-04-22 11:40:13 +0800

    增加镜像版本

--- sso-client2/build.sh
+++ sso-client2/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mvn -Dmaven.test.skip=true -U clean install
 
-docker build -t hub.c.163.com/longfeizheng/sso-client2 .
+docker build -t hub.c.163.com/longfeizheng/sso-client2:1.0 .
 
-docker push hub.c.163.com/longfeizheng/sso-client2
+#docker push hub.c.163.com/longfeizheng/sso-client2:1.0
--- sso-resource/build.sh
+++ sso-resource/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mvn -Dmaven.test.skip=true -U clean install
 
-docker build -t hub.c.163.com/longfeizheng/sso-resource .
+docker build -t hub.c.163.com/longfeizheng/sso-resource:1.0 .
 
-docker push hub.c.163.com/longfeizheng/sso-resource
+#docker push hub.c.163.com/longfeizheng/sso-resource:1.0
